db7a978d0a93e3107e7739263910387261931981
Junior Souza <jrsouzasolucoes@gmail.com>
2023-04-06 15:07:55 -0300

    Adicionar arquivos de projeto.

---
+++ Triangulo.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Triangulo.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ladoA = 7;
+            int ladoB = 7;
+            int ladoC = 7;
+
+            Triangulo triangulo = new Triangulo(ladoA, ladoB, ladoC);
+            string resultado = triangulo.VerificarTriangulo();
+
+            Console.WriteLine($"\nO triângulo é: {resultado}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Triangulo.ConsoleApp/Triangulo.cs
@@ -0,0 +1,39 @@
+﻿namespace Triangulo.ConsoleApp
+{
+    class Triangulo
+    {
+        public int LadoA;
+        public int LadoB;
+        public int LadoC;
+
+        public Triangulo(int ladoA, int ladoB, int ladoC)
+        {
+            this.LadoA = ladoA;
+            this.LadoB = ladoB;
+            this.LadoC = ladoC;
+        }
+
+        public string VerificarTriangulo()
+        {
+            if (LadoA < LadoB + LadoC && LadoB < LadoA + LadoC && LadoC < LadoA + LadoB)
+            {
+                if (LadoA == LadoB && LadoB == LadoC)
+                {
+                    return "Equilátero";
+                }
+                else if (LadoA == LadoB || LadoA == LadoC || LadoB == LadoC)
+                {
+                    return "Isósceles";
+                }
+                else
+                {
+                    return "Escaleno";
+                }
+            }
+            else
+            {
+                return "Não é um triângulo";
+            }
+        }
+    }
+}
